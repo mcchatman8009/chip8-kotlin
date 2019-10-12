9e2917269d319a676d765af666952c535d5fc3f5
Marlon Chatman <mcchatman8009@gmail.com>
2019-10-12 11:07:31 -0500

    Add better util methods

--- src/main/kotlin/chip8/entity/Chip8Byte.kt
+++ src/main/kotlin/chip8/entity/Chip8Byte.kt
@@ -9,4 +9,16 @@ inline class Chip8Byte(private val originalByte: Int) {
 
     val lowOrderNibble: Int
         get() = ((originalByte and 0xFF) and 0xF)
+
+    fun addTo(value: Int): Chip8Byte = Chip8Byte(originalByte + value)
+
+    fun addTo(byte: Chip8Byte): Chip8Byte = Chip8Byte(originalByte + byte.value)
+
+    fun subtract(byte: Chip8Byte): Chip8Byte = Chip8Byte(originalByte - byte.value)
+
+    fun xor(byte: Chip8Byte): Chip8Byte = Chip8Byte(originalByte xor byte.value)
+
+    fun or(byte: Chip8Byte): Chip8Byte = Chip8Byte(originalByte or byte.value)
+
+    fun and(byte: Chip8Byte): Chip8Byte = Chip8Byte(originalByte and byte.value)
 }
--- src/main/kotlin/chip8/util/Chip8Util.kt
+++ src/main/kotlin/chip8/util/Chip8Util.kt
@@ -1,10 +1,22 @@
 package chip8.util
 
+import chip8.cpu.Chip8Cpu
+import chip8.entity.Chip8Byte
 import chip8.entity.Chip8Word
 
 class Chip8Util {
 
     companion object {
+        fun getRegisterByteValueFromNumber(cpu: Chip8Cpu, registerNumber: Int): Chip8Byte {
+            val symbol = cpu.getRegisterByteSymbolByNumber(registerNumber)
+            return cpu.getByteRegisterValue(symbol)
+        }
+
+        fun setRegisterByteValueFromNumber(cpu: Chip8Cpu, registerNumber: Int, byte: Chip8Byte) {
+            val symbol = cpu.getRegisterByteSymbolByNumber(registerNumber)
+            cpu.setRegisterByteValue(symbol, byte)
+        }
+
         fun toWord(highByte: Int, lowByte: Int): Chip8Word =
             Chip8Word(((highByte shl 8) and 0xFF00) or (lowByte and 0xFF))
     }
